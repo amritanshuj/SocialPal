fb001789380c415683d48c5b3e4daf544bad6cb3
Amritanshu <amritanshuj.ic.16@nsit.net.in>
2020-08-08 04:30:22 +0530

    Setting up view engine

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = 8000;
 //Use express router
 app.use('/', require('./routes/index'));
 
-
+//Set up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
     if(err){
