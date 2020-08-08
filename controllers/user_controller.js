34ece765f596e0a75aade51e4bd3ab4625779a1b
Amritanshu <amritanshuj.ic.16@nsit.net.in>
2020-08-09 05:10:52 +0530

    Adding Sign-in/Sign-up pages

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,47 @@
+const User = require("../models/user");
+
 module.exports.profile = function(req, res){
-    res.render('users.ejs',{
+    res.render('users',{
         title: "Users"
     });
+}
+
+//Render sign-up page
+module.exports.signup = function(req, res){
+    return res.render('user_sign_up', {
+        title: "SocialPal | Sign Up"
+    })
+}
+
+//Render sign-in page
+module.exports.signin = function(req, res){
+    return res.render('user_sign_in', {
+        title: "SocialPal | Sign In"
+    })
+}
+
+//Get sign up data
+module.exports.create = function(req, res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('Error in finding user in signing up'); return}
+
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('Error in signing up user'); return}
+
+                return res.redirect('/users/sign-in');
+            })
+        }
+        else{
+            res.redirect('back');
+        }
+    })
+}
+//Sign In and create a session for the user
+module.exports.createSession = function(req, res){
+    //Todo
 }
--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,9 @@ const usersController = require('../controllers/user_controller');
 
 router.get('/profile', usersController.profile);
 
+router.get('/sign-up', usersController.signup);
+router.get('/sign-in', usersController.signin);
+
+router.post('/create', usersController.create);
+
 module.exports = router;
