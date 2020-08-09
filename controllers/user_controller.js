0efb87b926737b2cb0dfd09826ea967a026f27cb
Amritanshu <amritanshuj.ic.16@nsit.net.in>
2020-08-09 14:10:20 +0530

    Completing Manual Authentication

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,9 +1,19 @@
 const User = require("../models/user");
 
 module.exports.profile = function(req, res){
-    res.render('users',{
-        title: "Users"
-    });
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if(user){
+                res.render('users',{
+                    title: "User Profile",
+                    user: user
+                });
+            }
+        });
+    }else {
+        return res.redirect('/users/sign-in');
+    }  
+     
 }
 
 //Render sign-up page
@@ -43,5 +53,30 @@ module.exports.create = function(req, res){
 }
 //Sign In and create a session for the user
 module.exports.createSession = function(req, res){
-    //Todo
+    //find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('Error in finding user in signing in'); return}
+    
+
+    //handle user found
+    if(user){
+        //handle password which don't match
+        if(user.password != req.body.password){
+            return res.redirect('back');
+        }
+        //handle session creation
+        res.cookie('user_id', user.id);
+        return res.redirect('/users/profile');
+    }
+    else{
+        //handle user not found
+        return res.redirect('back');
+    }
+});
+}
+
+//Sign out
+module.exports.signout = function(req, res){
+    res.cookie('user_id', null);
+    return res.redirect('/users/sign-in');
 }
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,9 @@ router.get('/profile', usersController.profile);
 
 router.get('/sign-up', usersController.signup);
 router.get('/sign-in', usersController.signin);
+router.get('/sign-out', usersController.signout);
 
 router.post('/create', usersController.create);
+router.post('/create-session', usersController.createSession);
 
 module.exports = router;
