29afe26340c74cc02ee23d859fa86b4f57c3a6f4
Amritanshu <amritanshuj.ic.16@nsit.net.in>
2020-08-16 23:15:00 +0530

    Adding Google authentication

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const usersController = require('../controllers/user_controller');
-const passport = require('passport');
 
 router.get('/profile/:id', passport.checkAuthentication, usersController.profile); //profile page will only open if signed in, if not then sign in page opens
 router.post('/update/:id', passport.checkAuthentication, usersController.update); 
@@ -19,5 +19,10 @@ router.post('/create-session', passport.authenticate(
 ), usersController.createSession);
 
 router.get('/sign-out', usersController.destroySession);
+
+
+router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/users/sign-in'}), usersController.createSession);
+
 
 module.exports = router;
